3f10903eb2532786fcfdeb82d5847f6c81581624
Simona <gorneanusimona@gmail.com>
2018-03-19 17:40:17 +0200

    refactor

---
+++ variabile.js
@@ -0,0 +1,30 @@
+var pozitie;
+var pozitieD;
+
+var z1;
+var z2;
+
+var mutatZ1;
+var mutatZ2;
+
+var culoare = 0;
+var color = 1;
+
+var directie = 1;
+
+var nrmutari = 0;
+
+var m = 0;
+var n = 0;
+var nrpN = 0;
+var nrpA = 0;
+var vCuloare;
+
+
+var imgs = document.getElementsByTagName('img');
+var nrimgs = imgs.length;
+var ct = "nu";
+
+var coloana;
+var nrPieseColoana;
+var y;
